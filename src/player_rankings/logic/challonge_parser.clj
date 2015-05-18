f0aeecc8b2e4d58656f113e733a0b383c827795e
Shawn Krisman <shawn@everlane.com>
2015-05-17 20:56:27 -0700

    participants url creator.

--- src/player_rankings/logic/challonge_parser.clj
+++ src/player_rankings/logic/challonge_parser.clj
@@ -1,9 +1,14 @@
 (ns player-rankings.logic.challonge-parser
-  (:require [clojure.string :as string])
+  (:require [clojure.string :as string]
+            [player-rankings.secrets :refer [challonge-api-key]])
   (:import [java.net URL]))
 
-(defn create-base-api-url [url]
+(defn- create-base-api-url [url]
   (let [parseable-url (URL. url)
         subdomain (-> parseable-url .getHost (string/split #"\.") (get 0))
         tournament-name (-> parseable-url .getPath (string/replace "/" ""))]
-    (str "http://api.challonge.com/v1/tournaments/" subdomain "-" tournament-name)))
+    (str "http://api.challonge.com/v1/tournaments/" subdomain "-" tournament-name "/")))
+
+(defn- participants-url [url]
+  (let [base-url (create-base-api-url url)]
+    (str base-url "participants.json?api_key=" challonge-api-key)))
