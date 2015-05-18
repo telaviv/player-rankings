36bda9a6673fc838e921dd60be9a546c25f65538
Shawn Krisman <shawn@everlane.com>
2015-05-18 01:06:06 -0700

    finished challonge parser.

--- project.clj
+++ project.clj
@@ -19,6 +19,8 @@
                  [org.clojure/tools.nrepl "0.2.10"]
                  [ring-server "0.4.0"]
                  [clj-http "1.1.2"]
+                 [clojurewerkz/urly "1.0.0"]
+                 [org.clojure/data.json "0.2.6"]
                  ]
 
   :min-lein-version "2.0.0"
--- src/player_rankings/logic/challonge_parser.clj
+++ src/player_rankings/logic/challonge_parser.clj
@@ -1,6 +1,8 @@
 (ns player-rankings.logic.challonge-parser
   (:require [clojure.string :as string]
-            [player-rankings.secrets :refer [challonge-api-key]])
+            [player-rankings.secrets :refer [challonge-api-key]]
+            [clj-http.client :as client]
+            [clojure.data.json :as json])
   (:import [java.net URL]))
 
 (defn- create-base-api-url [url]
@@ -18,3 +20,30 @@
 
 (defn- matches-url [url]
   (create-url-by-type url "matches"))
+
+(defn- make-request [api-url]
+  (-> api-url client/get :body json/read-str))
+
+(defn- participant-name-by-id [id participants]
+  (let [participant (first (filter #(= id (get-in % ["participant" "id"])) participants))]
+    (get-in participant ["participant" "display_name"])))
+
+(defn- get-winner [match]
+  (let [core-match (match "match")]
+    (if (= (core-match "winner_id" ) (core-match "player1_id")) 1 2)))
+
+(defn- merge-matches-and-participants [matches participants]
+  (letfn [(participant-finder [key]
+            (fn [match]
+              (participant-name-by-id (get-in match ["match" key]) participants)))
+          (merge-match [match]
+            {:player-one ((participant-finder "player1_id") match)
+             :player-two ((participant-finder "player2_id") match)
+             :scores (get-in match ["match" "scores_csv"])
+             :winner (get-winner match)})]
+    (map merge-match matches)))
+
+(defn get-matches-from-url [url]
+  (let [matches (-> url matches-url make-request)
+        participants (-> url participants-url make-request)]
+    (merge-matches-and-participants matches participants)))
