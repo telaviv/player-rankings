cbf4aa6c417c7cf2c5cd29399e3a8b3a183965e7
Shawn Krisman <shawn@everlane.com>
2015-12-23 17:26:22 -0800

    finished crew battle stuff.

--- src/clj/player_rankings/logic/database.clj
+++ src/clj/player_rankings/logic/database.clj
@@ -229,20 +229,29 @@
        (sort-by :min)
        (reverse)))
 
-(defnp crew-eligible-for-ranking? [players existing-players]
-  (let [player-scores (map #(get-matching-player % existing-players) players)
-        eligible-players (filter #(< (:stddev %) 100) player-scores)]
-    (>= 5 (count eligible-players))))
+(defnp filtered-crew-members [crew existing-players]
+  (let [crew-scores (map #(get-matching-player % existing-players) crew)]
+    (filter #(< (:stddev %) 100) crew-scores)))
 
-(defnp filter-ineligible-crews [crews]
-  (let [player-names (-> constants/norcal-crews vals flatten)
+(defnp top-players-in-crew [crew-scores]
+  (take 5 (reverse (sort-by :rating crew-scores))))
+
+(defnp score-top-players-in-crew [crew-scores]
+  (/ (reduce + (map :rating (top-players-in-crew crew-scores))) 5))
+
+(defnp score-crews [crews existing-players]
+  (reduce-kv
+   (fn [coll crew-name crew]
+     (let [crew-scores (filtered-crew-members crew existing-players)]
+       (if (>= (count crew-scores) 5)
+         (assoc coll crew-name {:score (score-top-players-in-crew crew-scores)})
+         coll)))
+   {} crews))
+
+(defnp crew-scores [crews]
+  (let [player-names (-> crews vals flatten)
         existing-players (get-players-by-name-for-rank-sorting player-names)]
-    (reduce-kv
-     (fn [coll crew-name crew]
-       (if (crew-eligible-for-ranking? crew existing-players)
-         (assoc coll crew-name crew)
-         coll))
-     {} crews)))
+    (score-crews crews existing-players)))
 
 (defn- create-player-nodes [matches]
   (let [first-players (map :player-one matches)
