e2c2de4fa40546e4d4baf8e754982afe577f4a64
Shawn Krisman <krisman.shawn@gmail.com>
2016-05-18 10:22:21 -0700

    added an index on alias name.

--- src/clj/player_rankings/database/connection.clj
+++ src/clj/player_rankings/database/connection.clj
@@ -1,6 +1,9 @@
 (ns player-rankings.database.connection
   (:require [clojurewerkz.neocons.rest :as nr]
+            [clojurewerkz.neocons.rest.cypher :as cypher]
             [player-rankings.secrets :refer [neo4j-username neo4j-password]]))
 
 (def conn (nr/connect
            (str "http://" neo4j-username ":" neo4j-password "@localhost:7474/db/data/")))
+
+(cypher/tquery conn "create index on :alias(name)")
