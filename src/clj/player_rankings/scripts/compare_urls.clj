7abdcecdb1c5aa396cf82f05d93bcde5cab303c1
Shawn Krisman <krisman.shawn@gmail.com>
2017-02-22 00:15:01 -0800

    better normalize urls.

--- src/clj/player_rankings/scripts/compare_urls.clj
+++ src/clj/player_rankings/scripts/compare_urls.clj
@@ -1,9 +1,10 @@
 (ns player-rankings.scripts.compare-urls
   (:require [clojure.set :refer [difference]]
             [clojure.string :as str]
-            [cemerick.url :refer [url]]
             [clj-http.client :as client]
             [net.cgrand.enlive-html :as html]
+            [player-rankings.parsers.smashgg :as smashgg]
+            [player-rankings.parsers.challonge :as challonge]
             [player-rankings.logic.tournament-constants :refer [tournament-urls]]))
 
 (def NORCAL-TOURNAMENT-URL
@@ -24,24 +25,33 @@
        (map (fn [elem] (-> elem :attrs :href)))))
 
 (defn normalized-old-tournament-urls []
-  (map (fn [url]
-         (if (string? url)
-           url
-           (:url url)))
-       tournament-urls))
+  (set (map (fn [url]
+              (if (string? url)
+                url
+                (:url url)))
+            tournament-urls)))
 
 (defn new-tournament-urls []
   (map (fn [gurl]
          (-> gurl url :query (get "q")))
        (norcal-google-redirects)))
 
+(defn normalized-new-tournament-urls []
+  (reduce (fn [urls url]
+            (let [normalized (normalize-url url)]
+              (if (nil? normalized)
+                urls
+                (conj urls normalized))))
+          #{} (new-tournament-urls)))
+
 (defn unadded-urls []
-  (let [old-urls (normalized-old-tournament-urls)]
-    (filter
-     (fn [new-url]
-       (not (some #(str/starts-with? (str/lower-case %) (str/lower-case new-url))
-                  (concat NEW-URL-BLACKLIST old-urls))))
-     (new-tournament-urls))))
+  (difference (normalized-new-tournament-urls)
+              (normalized-old-tournament-urls)))
+
+(defn normalize-url [url]
+  (cond (smashgg/matching-url? url) (smashgg/normalize-url url)
+        (challonge/matching-url? url) (challonge/normalize-url url)
+        :else nil))
 
 (defn print-missing-urls []
-  (doseq [url (sort (unadded-urls))] (println url)))
+  (doseq [url (sort (unadded-urls))] (println (format "\"%s\"" url))))
