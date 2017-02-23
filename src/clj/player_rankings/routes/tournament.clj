4fa41e22e7bca70ca1231eb6b6d4c2a6fb0b7d71
Shawn Krisman <krisman.shawn@gmail.com>
2017-02-23 00:05:32 -0800

    add validation to the tournament page.

--- src/clj/player_rankings/routes/tournament.clj
+++ src/clj/player_rankings/routes/tournament.clj
@@ -1,11 +1,13 @@
 (ns player-rankings.routes.tournament
   (:require [compojure.core :refer [defroutes GET]]
+            [schema.core :as s]
+            [player-rankings.routes.schema :refer [TournamentPage]]
             [player-rankings.database.tournaments.read :as tournament]))
 
 (defn tournament-info [req]
   (let [id (get-in req [:params :id])]
     (if (tournament/tournament-exists? id)
-      {:status 200 :body (tournament/tournament-data (get-in req [:params :id]))}
+      {:status 200 :body (s/validate TournamentPage (tournament/tournament-data (get-in req [:params :id])))}
       {:status 400 :body {:errors {:name  {:msg (str "We found no record of " id)}}}})))
 
 (defroutes tournament-routes
